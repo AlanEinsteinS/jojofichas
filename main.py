ff0ed41a5694c8f9afd65e5604244e5459a508e8
AlanEinsteinS <alaneinstein245@gmail.com>
2024-06-26 13:37:54 -0300

    patch 1.4.1

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ st.session_state.ficha["nome_stand"] = st.text_input("Nome do stand", st.session
 st.header("Atributos")
 rank_choices = ["E", "D", "C", "B", "A", "S", "S+", "X"]
 cols = st.columns(5)
-for i, atributo in enumerate(["forca", "velocidade", "precisao", "durabilidade", "potencia"]):
+for i, atributo in enumerate(["forca", "velocidade", "precisao", "durabilidade", "potencial"]):
     st.session_state.ficha["atributos"][atributo] = cols[i].selectbox(atributo.capitalize(), rank_choices, index=rank_choices.index(st.session_state.ficha["atributos"][atributo]))
 
 # Health, Stamina, Impulse, and Willpower Bars
